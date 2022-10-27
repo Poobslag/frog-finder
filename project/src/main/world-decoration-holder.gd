83d5956c95bcae0a6eb5be8cbd312fdf7c11f1ef
Poobslag <aaron.pieper@gmail.com>
2022-10-27 12:42:18 -0400

    Added world decorations, decoration cycling logic

---
+++ project/src/main/world-decoration-holder.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready() -> void:
+	_refresh_world_index()
+	PlayerData.connect("world_index_changed", self, "_on_PlayerData_world_index_changed")
+
+
+## Ensure exactly one set of world decorations is visible.
+func _refresh_world_index() -> void:
+	var new_world_index := PlayerData.world_index
+	if new_world_index == -1:
+		new_world_index = 0
+	
+	for i in range(get_child_count()):
+		get_child(i).visible = true if i == new_world_index else false
+
+
+func _on_PlayerData_world_index_changed() -> void:
+	_refresh_world_index()
