3a96946c81f908c9c056e9e3ec16bc8e58c914bc
Poobslag <aaron.pieper@gmail.com>
2022-12-15 14:44:41 -0500

    Added difficulty label to LevelRulesDemo

--- project/src/demo/level-rules-demo.gd
+++ project/src/demo/level-rules-demo.gd
@@ -6,12 +6,14 @@ extends Node
 ## 	[=/-]: Increases/decreases the puzzle difficulty and generates a new puzzle.
 
 onready var _gameplay_panel := $GameplayPanel
+onready var _difficulty_label := $DifficultyLabel
 
 export (PackedScene) var LevelRulesScene: PackedScene
 
 func _ready() -> void:
 	randomize()
 	_gameplay_panel.mission_string = "1-1"
+	_refresh_difficulty_label()
 
 
 func _input(event: InputEvent) -> void:
@@ -26,8 +28,13 @@ func _input(event: InputEvent) -> void:
 			_show_puzzle()
 
 
+func _refresh_difficulty_label() -> void:
+	_difficulty_label.text = tr("Difficulty: %s" % [_gameplay_panel.player_puzzle_difficulty])
+
+
 func _show_puzzle() -> void:
 	_gameplay_panel.level_ids = ["evasive-touch"]
 	_gameplay_panel.level_rules_scenes_by_id = {"evasive-touch": LevelRulesScene}
 	_gameplay_panel.reset()
 	_gameplay_panel.show_puzzle()
+	_refresh_difficulty_label()
