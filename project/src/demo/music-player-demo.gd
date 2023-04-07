141b343efa9cfa4158b6d6470dd3317aab22217b
Poobslag <aaron.pieper@gmail.com>
2023-04-07 15:51:26 -0400

    New demos: BackgroundDemo, MusicPlayerDemo
    These functions are behaving strangely after the Godot 4 upgrade, having
    a demo makes them easier to test.

---
+++ project/src/demo/music-player-demo.gd
@@ -0,0 +1,24 @@
+extends Node
+## Demonstrates the music player.
+##
+## Keys:
+## 	[D]: Play a dance song
+## 	[E]: Play an ending song
+## 	[P]: Play a normal level song
+## 	[S]: Play shark song
+
+func _ready():
+	PlayerData.music_preference = PlayerData.MusicPreference.RANDOM
+	PlayerData.world_index = 0
+
+
+func _input(event: InputEvent) -> void:
+	match Utils.key_scancode(event):
+		KEY_D:
+			MusicPlayer.play_dance_song()
+		KEY_E:
+			MusicPlayer.play_ending_song()
+		KEY_P:
+			MusicPlayer.play_preferred_song()
+		KEY_S:
+			MusicPlayer.play_shark_song()
