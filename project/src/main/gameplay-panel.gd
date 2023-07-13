e4f3a651a6da6e2e49e588fe51c679d3e59ba004
Poobslag <aaron.pieper@gmail.com>
2023-07-13 11:25:27 -0400

    Refactor: FooLevelRules -> FooRules
    Renamed various 'LevelRules' classes to 'Rules'. Moved them into their
    own folder.

--- project/src/main/gameplay-panel.gd
+++ project/src/main/gameplay-panel.gd
@@ -59,13 +59,13 @@ var _start_difficulty := 0
 ## key: (String) level id
 ## value: (PackedScene) scene with LevelRules for the specified level
 @onready var level_rules_scenes_by_id := {
-	"secret-collect": preload("res://src/main/SecretCollectLevelRules.tscn"),
-	"froggo": preload("res://src/main/FroggoLevelRules.tscn"),
-	"frodoku": preload("res://src/main/FrodokuLevelRules.tscn"),
-	"maze": preload("res://src/main/MazeLevelRules.tscn"),
-	"word-find": preload("res://src/main/WordFindLevelRules.tscn"),
-	"pattern-memory": preload("res://src/main/PatternMemoryLevelRules.tscn"),
-	"fruit-maze": preload("res://src/main/FruitMazeRules.tscn"),
+	"secret-collect": preload("res://src/main/levels/SecretCollectRules.tscn"),
+	"froggo": preload("res://src/main/levels/FroggoRules.tscn"),
+	"frodoku": preload("res://src/main/levels/FrodokuRules.tscn"),
+	"maze": preload("res://src/main/levels/MazeRules.tscn"),
+	"word-find": preload("res://src/main/levels/WordFindRules.tscn"),
+	"pattern-memory": preload("res://src/main/levels/PatternMemoryRules.tscn"),
+	"fruit-maze": preload("res://src/main/levels/FruitMazeRules.tscn"),
 }
 
 func _ready() -> void:
