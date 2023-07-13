Poobslag/frog-finder

commit 64c8db6241689286c30e4050a53fd7cd4554c773

Refactor: IntermissionPanel/CardControl onready, reordering
Extracted onready fields for IntermissionPanel/CardControl fields, to avoid
unnecessary get_node() calls

Reordered/alphabetized CardControl fields and nodes.

project/src/main/card-control.gd
@@ -159,8 +159,19 @@ var _shark_sounds := [
 	preload("res://assets/main/sfx/shark-voice-7.wav"),
 ]
 
+var _game_state: GameState
+var _pending_warning := ""
+
 onready var _card_back_sprite := $CardBack
 onready var _card_front_sprite := $CardFront
+
+onready var _cheer_timer := $CheerTimer
+onready var _frog_found_timer := $FrogFoundTimer
+onready var _shark_found_timer := $SharkFoundTimer
+onready var _stop_dance_timer := $StopDanceTimer
+
+onready var _creature_sfx := $CreatureSfx
+onready var _pop_brust_sfx := $PopBrustSfx
 
 onready var _frog_sheet := preload("res://assets/main/frog-frame-00-sheet.png")
 onready var _letter_sheet := preload("res://assets/main/letter-sheet.png")
@@ -171,9 +182,6 @@ onready var _lizard_sheet := preload("res://assets/main/lizard-sheet.png")
 onready var _arrow_sheet := preload("res://assets/main/arrow-sheet.png")
 onready var _hex_arrow_sheet := preload("res://assets/main/hex-arrow-sheet.png")
 onready var _fruit_sheet := preload("res://assets/main/fruit-sheet.png")
-onready var _game_state: GameState
-
-var _pending_warning := ""
 
 func _ready() -> void:
 	_refresh_card_textures()
@@ -318,9 +326,9 @@ func reset() -> void:
 	card_front_type = CardType.MYSTERY
 	card_front_details = ""
 	
-	$StopDanceTimer.stop()
-	$FrogFoundTimer.stop()
-	$SharkFoundTimer.stop()
+	_stop_dance_timer.stop()
+	_frog_found_timer.stop()
+	_shark_found_timer.stop()
 	_refresh_card_textures()
 
 
@@ -329,26 +337,26 @@ func is_front_shown() -> bool:
 
 
 func show_front() -> void:
-	if $CardFront.visible:
+	if _card_front_sprite.visible:
 		# already shown
 		return
 	
 	# can't reference _card_back and _card_front fields. show_front() sometimes precedes _ready()
-	$CardBack.visible = false
-	$CardFront.visible = true
+	_card_back_sprite.visible = false
+	_card_front_sprite.visible = true
 	
 	# reset the wiggle so the characters don't have one very abbreviated dance frame
-	$CardFront.reset_wiggle()
+	_card_front_sprite.reset_wiggle()
 
 
 func hide_front() -> void:
-	if not $CardFront.visible:
+	if not _card_front_sprite.visible:
 		# already hidden
 		return
 	
 	# can't reference _card_back and _card_front fields. show_front() sometimes precedes _ready()
-	$CardBack.visible = true
-	$CardFront.visible = false
+	_card_back_sprite.visible = true
+	_card_front_sprite.visible = false
 
 
 func copy_from(other_card: CardControl) -> void:
@@ -380,8 +388,8 @@ func _flip_card() -> void:
 		# the level hasn't started, or the level has ended
 		return
 	
-	$PopBrustSfx.pitch_scale = rand_range(0.9, 1.10)
-	$PopBrustSfx.play()
+	_pop_brust_sfx.pitch_scale = rand_range(0.9, 1.10)
+	_pop_brust_sfx.play()
 	emit_signal("before_card_flipped")
 	
 	show_front()
@@ -400,7 +408,7 @@ func cheer() -> void:
 	
 	# reset the wiggle so the characters don't have one very abbreviated dance frame
 	_card_front_sprite.reset_wiggle()
-	$StopDanceTimer.start(4.0)
+	_stop_dance_timer.start(4.0)
 
 
 func _on_FlipTimer_timeout() -> void:
@@ -414,8 +422,8 @@ func _on_FlipTimer_timeout() -> void:
 				_game_state.can_interact = false
 			
 			emit_signal("before_frog_found")
-			$CheerTimer.start()
-			$FrogFoundTimer.start(2.0)
+			_cheer_timer.start()
+			_frog_found_timer.start(2.0)
 		CardType.SHARK:
 			if practice:
 				# this shark doesn't count; maybe it was on the main menu
@@ -423,10 +431,10 @@ func _on_FlipTimer_timeout() -> void:
 			else:
 				_game_state.can_interact = false
 			emit_signal("before_shark_found")
-			$SharkFoundTimer.start(3.2)
-			$CreatureSfx.stream = Utils.rand_value(_shark_sounds)
-			$CreatureSfx.pitch_scale = rand_range(0.8, 1.2)
-			$CreatureSfx.play()
+			_shark_found_timer.start(3.2)
+			_creature_sfx.stream = Utils.rand_value(_shark_sounds)
+			_creature_sfx.pitch_scale = rand_range(0.8, 1.2)
+			_creature_sfx.play()
 
 
 func _on_StopDanceTimer_timeout() -> void:
@@ -446,7 +454,7 @@ func _on_SharkFoundTimer_timeout() -> void:
 
 
 func _on_CheerTimer_timeout() -> void:
-	$CreatureSfx.stream = Utils.rand_value(_frog_sounds)
-	$CreatureSfx.pitch_scale = rand_range(0.8, 1.2)
-	$CreatureSfx.play()
+	_creature_sfx.stream = Utils.rand_value(_frog_sounds)
+	_creature_sfx.pitch_scale = rand_range(0.8, 1.2)
+	_creature_sfx.play()
 	cheer()

project/src/main/CardControl.tscn
@@ -12,31 +12,29 @@ script = ExtResource( 3 )
 game_state_path = NodePath("")
 
 [node name="CardBack" parent="." instance=ExtResource( 2 )]
-scale = Vector2( 0.333, 0.333 )
 
 [node name="CardFront" parent="." instance=ExtResource( 2 )]
 visible = false
-scale = Vector2( 0.333, 0.333 )
-
-[node name="StopDanceTimer" type="Timer" parent="."]
-one_shot = true
-
-[node name="SharkFoundTimer" type="Timer" parent="."]
-one_shot = true
-
-[node name="FrogFoundTimer" type="Timer" parent="."]
-one_shot = true
 
 [node name="CheerTimer" type="Timer" parent="."]
 wait_time = 0.5
 one_shot = true
 
-[node name="PopBrustSfx" type="AudioStreamPlayer" parent="."]
-stream = ExtResource( 1 )
-volume_db = -10.0
+[node name="FrogFoundTimer" type="Timer" parent="."]
+one_shot = true
+
+[node name="SharkFoundTimer" type="Timer" parent="."]
+one_shot = true
+
+[node name="StopDanceTimer" type="Timer" parent="."]
+one_shot = true
 
 [node name="CreatureSfx" type="AudioStreamPlayer" parent="."]
 volume_db = -8.0
+
+[node name="PopBrustSfx" type="AudioStreamPlayer" parent="."]
+stream = ExtResource( 1 )
+volume_db = -10.0
 
 [connection signal="timeout" from="CheerTimer" to="." method="_on_CheerTimer_timeout"]
 [connection signal="timeout" from="FrogFoundTimer" to="." method="_on_FrogFoundTimer_timeout"]

project/src/main/intermission-panel.gd
@@ -38,9 +38,14 @@ var next_card_index := 0
 var RunningSharkScene := preload("res://src/main/RunningShark.tscn")
 var RunningFrogScene := preload("res://src/main/RunningFrog.tscn")
 
-onready var _intermission_cards: LevelCards = $IntermissionCards
 onready var hand: Hand = get_node(hand_path)
+
 onready var _bye_button := $ByeButton
+onready var _creatures := $Creatures
+onready var _creatures_ysort := $Creatures/YSort
+onready var _frog_spawn_timer := $FrogSpawnTimer
+onready var _intermission_cards: LevelCards = $IntermissionCards
+onready var _shark_spawn_timer := $SharkSpawnTimer
 
 func _ready() -> void:
 	hand.connect("hug_finished", self, "_on_Hand_hug_finished")
@@ -62,12 +67,12 @@ func restart(mission_string: String) -> void:
 
 
 func reset() -> void:
-	$SharkSpawnTimer.stop()
-	$FrogSpawnTimer.stop()
+	_shark_spawn_timer.stop()
+	_frog_spawn_timer.stop()
 	_bye_button.visible = false
-	for child in $Creatures/YSort.get_children():
+	for child in _creatures_ysort.get_children():
 		child.queue_free()
-		$Creatures/YSort.remove_child(child)
+		_creatures_ysort.remove_child(child)
 	sharks.clear()
 	frogs.clear()
 
@@ -89,7 +94,7 @@ func show_intermission_panel() -> void:
 
 func start_shark_spawn_timer(biteable_fingers: int = 1) -> void:
 	hand.biteable_fingers = biteable_fingers
-	$SharkSpawnTimer.start()
+	_shark_spawn_timer.start()
 	for _finger in range(biteable_fingers):
 		# spawn one shark per finger
 		_spawn_shark(true)
@@ -98,7 +103,7 @@ func start_shark_spawn_timer(biteable_fingers: int = 1) -> void:
 func start_frog_hug_timer(huggable_fingers: int, new_max_frogs: int) -> void:
 	max_frogs = new_max_frogs
 	hand.huggable_fingers = huggable_fingers
-	$FrogSpawnTimer.start()
+	_frog_spawn_timer.start()
 	for _finger in range(huggable_fingers):
 		# spawn one frog per finger
 		var frog := _spawn_frog(true)
@@ -116,7 +121,7 @@ func start_frog_dance(frog_count: int) -> void:
 	
 	# frog runs into position, dances and leaves
 	for i in range(frogs.size()):
-		var dance_target := Rect2(Vector2.ZERO, $Creatures.rect_size).get_center()
+		var dance_target := Rect2(Vector2.ZERO, _creatures.rect_size).get_center()
 		dance_target += arrangement[i] * Vector2(64, 48)
 		
 		_dance(frogs[i], dance_target)
@@ -147,8 +152,8 @@ func _spawn_shark(away_from_hand: bool = false) -> void:
 		# this shark has a friend
 		shark.friend = sharks[sharks.size() - 1]
 	
-	shark.soon_position = spawn_points[0] - $Creatures.rect_global_position
-	$Creatures/YSort.add_child(shark)
+	shark.soon_position = spawn_points[0] - _creatures.rect_global_position
+	_creatures_ysort.add_child(shark)
 	sharks.append(shark)
 	shark.chase()
 
@@ -177,8 +182,8 @@ func _spawn_frog(away_from_hand: bool = false) -> RunningFrog:
 		# this frog has a friend
 		friends_by_frog[frog] = frogs[frogs.size() - 1]
 	
-	frog.soon_position = spawn_points[0] - $Creatures.rect_global_position
-	$Creatures/YSort.add_child(frog)
+	frog.soon_position = spawn_points[0] - _creatures.rect_global_position
+	_creatures_ysort.add_child(frog)
 	frogs.append(frog)
 	return frog
 
@@ -207,7 +212,7 @@ func _on_SharkSpawnTimer_timeout() -> void:
 	var shark_delay_index := sharks.size() - 1
 	if shark_delay_index < SHARK_DELAYS.size() and hand.biteable_fingers >= 1:
 		_spawn_shark(true)
-		$SharkSpawnTimer.start(SHARK_DELAYS[shark_delay_index])
+		_shark_spawn_timer.start(SHARK_DELAYS[shark_delay_index])
 
 
 func _on_FrogSpawnTimer_timeout() -> void:
@@ -221,7 +226,7 @@ func _on_FrogSpawnTimer_timeout() -> void:
 	if frog_delay_index < FROG_DELAYS.size() and frogs.size() < max_frogs:
 		var frog := _spawn_frog(true)
 		_chase(frog)
-		$FrogSpawnTimer.start(FROG_DELAYS[frog_delay_index])
+		_frog_spawn_timer.start(FROG_DELAYS[frog_delay_index])
 
 
 func _on_Hand_hug_finished() -> void:

commit 3ed1bdbd0ccf50ca2f0fc5484b45a2462d5ad466

Refactor: CardControl.shown_face
64c8db62416892 broke a lot of code because it called show_front before the
onready variables were initialized. We fixed this in the short term by moving
the show_front calls after the card is initialized, but a better design is
to have a field like front_shown and back_shown which you set, and which then
show the appropriate sprite in _ready().

project/src/main/card-control.gd
@@ -8,6 +8,11 @@ signal frog_found
 signal before_shark_found
 signal shark_found
 
+enum CardFace {
+	BACK,
+	FRONT,
+}
+
 enum CardType {
 	NONE,
 	FROG,
@@ -137,6 +142,8 @@ const LIZARD_COUNT := 32
 @export var game_state_path := NodePath("../GameState") : set = set_game_state_path
 @export var practice := false
 
+@export var shown_face := CardFace.BACK : set = set_shown_face
+
 var _frog_sounds := [
 	preload("res://assets/main/sfx/frog-voice-0.wav"),
 	preload("res://assets/main/sfx/frog-voice-1.wav"),
@@ -186,6 +193,7 @@ var _pending_warning := ""
 func _ready() -> void:
 	_refresh_card_textures()
 	_refresh_game_state_path()
+	_refresh_shown_face()
 
 
 func _process(_delta: float) -> void:
@@ -197,6 +205,28 @@ func _process(_delta: float) -> void:
 func set_game_state_path(new_game_state_path: NodePath) -> void:
 	game_state_path = new_game_state_path
 	_refresh_game_state_path()
+
+
+func set_shown_face(new_shown_face: CardFace) -> void:
+	shown_face = new_shown_face
+	_refresh_shown_face()
+
+
+func _refresh_shown_face() -> void:
+	if not is_inside_tree():
+		return
+	
+	if shown_face == CardFace.BACK and _card_back_sprite.visible \
+			or shown_face == CardFace.FRONT and _card_front_sprite.visible:
+		# appropriate card face is already shown
+		return
+	
+	_card_back_sprite.visible = true if shown_face == CardFace.BACK else false
+	_card_front_sprite.visible = true if shown_face == CardFace.FRONT else false
+	
+	if shown_face == CardFace.FRONT:
+		# reset the wiggle so the characters don't have one very abbreviated dance frame
+		_card_front_sprite.reset_wiggle()
 
 
 func _refresh_game_state_path() -> void:
@@ -337,26 +367,11 @@ func is_front_shown() -> bool:
 
 
 func show_front() -> void:
-	if _card_front_sprite.visible:
-		# already shown
-		return
-	
-	# can't reference _card_back and _card_front fields. show_front() sometimes precedes _ready()
-	_card_back_sprite.visible = false
-	_card_front_sprite.visible = true
-	
-	# reset the wiggle so the characters don't have one very abbreviated dance frame
-	_card_front_sprite.reset_wiggle()
+	shown_face = CardFace.FRONT
 
 
 func hide_front() -> void:
-	if not _card_front_sprite.visible:
-		# already hidden
-		return
-	
-	# can't reference _card_back and _card_front fields. show_front() sometimes precedes _ready()
-	_card_back_sprite.visible = true
-	_card_front_sprite.visible = false
+	shown_face = CardFace.BACK
 
 
 func copy_from(other_card: CardControl) -> void: