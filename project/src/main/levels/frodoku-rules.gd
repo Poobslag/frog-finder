Poobslag/frog-finder

commit b297a70406b68accbee37d20243f8c99659ea4f8

Refactor: public methods before private

project/src/main/levels/frodoku-rules.gd
@@ -197,15 +197,15 @@ func _conflicting_lizard(card: CardControl, method: String = "") -> CardControl:
 	return result
 
 
-func compare_by_column(pos_1: Vector2, pos_2: Vector2) -> bool:
+func _compare_by_column(pos_1: Vector2, pos_2: Vector2) -> bool:
 	return pos_1.x == pos_2.x
 
 
-func compare_by_row(pos_1: Vector2, pos_2: Vector2) -> bool:
+func _compare_by_row(pos_1: Vector2, pos_2: Vector2) -> bool:
 	return pos_1.y == pos_2.y
 
 
-func compare_by_region(pos_1: Vector2, pos_2: Vector2) -> bool:
+func _compare_by_region(pos_1: Vector2, pos_2: Vector2) -> bool:
 	return _region(pos_1) == _region(pos_2)
 
 

project/src/main/card-control.gd
@@ -196,6 +196,11 @@ func _ready() -> void:
 	_refresh_shown_face()
 
 
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_mask & MOUSE_BUTTON_LEFT:
+		_flip_card()
+
+
 func _process(_delta: float) -> void:
 	if _pending_warning:
 		push_warning(_pending_warning)
@@ -210,6 +215,82 @@ func set_game_state_path(new_game_state_path: NodePath) -> void:
 func set_shown_face(new_shown_face: CardFace) -> void:
 	shown_face = new_shown_face
 	_refresh_shown_face()
+
+
+func set_card_back_type(new_card_back_type: CardType) -> void:
+	card_back_type = new_card_back_type
+	_refresh_card_textures()
+
+
+func set_card_front_type(new_card_front_type: CardType) -> void:
+	card_front_type = new_card_front_type
+	_refresh_card_textures()
+
+
+func set_card_back_details(new_card_back_details: String) -> void:
+	card_back_details = new_card_back_details
+	_refresh_card_textures()
+
+
+func set_card_front_details(new_card_front_details: String) -> void:
+	card_front_details = new_card_front_details
+	_refresh_card_textures()
+
+
+func reset() -> void:
+	_card_back_sprite.visible = true
+	_card_front_sprite.visible = false
+	card_back_type = CardType.MYSTERY
+	card_back_details = ""
+	card_front_type = CardType.MYSTERY
+	card_front_details = ""
+	
+	_stop_dance_timer.stop()
+	_frog_found_timer.stop()
+	_shark_found_timer.stop()
+	_refresh_card_textures()
+
+
+func is_front_shown() -> bool:
+	return _card_front_sprite.visible
+
+
+func show_front() -> void:
+	shown_face = CardFace.FRONT
+
+
+func hide_front() -> void:
+	shown_face = CardFace.BACK
+
+
+func copy_from(other_card: CardControl) -> void:
+	card_back_type = other_card.card_back_type
+	card_back_details = other_card.card_back_details
+	card_front_type = other_card.card_front_type
+	card_front_details = other_card.card_front_details
+	_refresh_card_textures()
+	
+	_card_front_sprite.frame = other_card._card_front_sprite.frame
+	_card_front_sprite.wiggle_frames = other_card._card_front_sprite.wiggle_frames
+	_card_front_sprite.visible = other_card._card_front_sprite.visible
+	
+	_card_back_sprite.frame = other_card._card_back_sprite.frame
+	_card_back_sprite.wiggle_frames = other_card._card_back_sprite.wiggle_frames
+	_card_back_sprite.visible = other_card._card_back_sprite.visible
+
+
+func cheer() -> void:
+	if card_front_type != CardType.FROG:
+		return
+	
+	var frog_index := int(_card_front_sprite.wiggle_frames[0] / 4)
+	_card_front_sprite.wiggle_frames = [frog_index * 4 + 2, frog_index * 4 + 3] as Array[int]
+	var wiggle_index: int = _card_front_sprite.frame % _card_front_sprite.wiggle_frames.size()
+	_card_front_sprite.frame = _card_front_sprite.wiggle_frames[wiggle_index]
+	
+	# reset the wiggle so the characters don't have one very abbreviated dance frame
+	_card_front_sprite.reset_wiggle()
+	_stop_dance_timer.start(4.0)
 
 
 func _refresh_shown_face() -> void:
@@ -323,73 +404,6 @@ func _refresh_card_face(card_sprite: Sprite2D, card_type: int, card_details: Str
 		card_sprite.frame = card_sprite.wiggle_frames[0]
 
 
-func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_mask & MOUSE_BUTTON_LEFT:
-		_flip_card()
-
-
-func set_card_back_type(new_card_back_type: CardType) -> void:
-	card_back_type = new_card_back_type
-	_refresh_card_textures()
-
-
-func set_card_front_type(new_card_front_type: CardType) -> void:
-	card_front_type = new_card_front_type
-	_refresh_card_textures()
-
-
-func set_card_back_details(new_card_back_details: String) -> void:
-	card_back_details = new_card_back_details
-	_refresh_card_textures()
-
-
-func set_card_front_details(new_card_front_details: String) -> void:
-	card_front_details = new_card_front_details
-	_refresh_card_textures()
-
-
-func reset() -> void:
-	_card_back_sprite.visible = true
-	_card_front_sprite.visible = false
-	card_back_type = CardType.MYSTERY
-	card_back_details = ""
-	card_front_type = CardType.MYSTERY
-	card_front_details = ""
-	
-	_stop_dance_timer.stop()
-	_frog_found_timer.stop()
-	_shark_found_timer.stop()
-	_refresh_card_textures()
-
-
-func is_front_shown() -> bool:
-	return _card_front_sprite.visible
-
-
-func show_front() -> void:
-	shown_face = CardFace.FRONT
-
-
-func hide_front() -> void:
-	shown_face = CardFace.BACK
-
-
-func copy_from(other_card: CardControl) -> void:
-	card_back_type = other_card.card_back_type
-	card_back_details = other_card.card_back_details
-	card_front_type = other_card.card_front_type
-	card_front_details = other_card.card_front_details
-	_refresh_card_textures()
-	
-	_card_front_sprite.frame = other_card._card_front_sprite.frame
-	_card_front_sprite.wiggle_frames = other_card._card_front_sprite.wiggle_frames
-	_card_front_sprite.visible = other_card._card_front_sprite.visible
-	
-	_card_back_sprite.frame = other_card._card_back_sprite.frame
-	_card_back_sprite.wiggle_frames = other_card._card_back_sprite.wiggle_frames
-	_card_back_sprite.visible = other_card._card_back_sprite.visible
-
-
 func _flip_card() -> void:
 	if _card_front_sprite.visible:
 		# card is already flipped
@@ -410,20 +424,6 @@ func _flip_card() -> void:
 	show_front()
 	_game_state.flip_timer.start()
 	_game_state.flip_timer.connect("timeout",Callable(self,"_on_FlipTimer_timeout"))
-
-
-func cheer() -> void:
-	if card_front_type != CardType.FROG:
-		return
-	
-	var frog_index := int(_card_front_sprite.wiggle_frames[0] / 4)
-	_card_front_sprite.wiggle_frames = [frog_index * 4 + 2, frog_index * 4 + 3] as Array[int]
-	var wiggle_index: int = _card_front_sprite.frame % _card_front_sprite.wiggle_frames.size()
-	_card_front_sprite.frame = _card_front_sprite.wiggle_frames[wiggle_index]
-	
-	# reset the wiggle so the characters don't have one very abbreviated dance frame
-	_card_front_sprite.reset_wiggle()
-	_stop_dance_timer.start(4.0)
 
 
 func _on_FlipTimer_timeout() -> void:

project/src/main/music-player.gd
@@ -107,27 +107,6 @@ func play_preferred_song() -> void:
 	_play_song(new_song)
 
 
-func _play_song(new_song: AudioStreamPlayer) -> void:
-	if _current_song and _current_song != new_song:
-		fade_out()
-	var previous_song := _current_song
-	_current_song = new_song
-	if _current_song != previous_song:
-		if _current_song:
-			var from_position: float = _position_by_song.get(_current_song, 0)
-			
-			# This line often triggers a warning because of Godot #75762
-			# (https://github.com/godotengine/godot/issues/75762). There is no known workaround.
-			_current_song.play(from_position)
-			
-			if from_position != 0:
-				# sample when playing a song from the middle, to avoid pops and clicks
-				_current_song.volume_db = MIN_VOLUME
-				_fade(_current_song, MAX_VOLUME, FADE_IN_DURATION)
-			else:
-				_current_song.volume_db = MAX_VOLUME
-
-
 func play_shark_song() -> void:
 	var shark_song: AudioStreamPlayer = _shark_song_by_world_index.get(PlayerData.world_index, _default_shark_song)
 	_play_song(shark_song)
@@ -187,6 +166,27 @@ func get_playback_position() -> float:
 	if _current_song:
 		result = _current_song.get_playback_position()
 	return result
+
+
+func _play_song(new_song: AudioStreamPlayer) -> void:
+	if _current_song and _current_song != new_song:
+		fade_out()
+	var previous_song := _current_song
+	_current_song = new_song
+	if _current_song != previous_song:
+		if _current_song:
+			var from_position: float = _position_by_song.get(_current_song, 0)
+			
+			# This line often triggers a warning because of Godot #75762
+			# (https://github.com/godotengine/godot/issues/75762). There is no known workaround.
+			_current_song.play(from_position)
+			
+			if from_position != 0:
+				# sample when playing a song from the middle, to avoid pops and clicks
+				_current_song.volume_db = MIN_VOLUME
+				_fade(_current_song, MAX_VOLUME, FADE_IN_DURATION)
+			else:
+				_current_song.volume_db = MAX_VOLUME
 
 
 ## Slowly apply a fade in or fade out effect to the specified song.

commit bd951a7846e91508fe1270d3a9a806d59c2dd137

Fixed errors in Frodoku level.
This was broken in b297a704 when the methods were renamed without updating all
of the references.

project/src/main/levels/frodoku-rules.gd
@@ -104,7 +104,7 @@ func add_cards() -> void:
 	
 	_reveal_lizards(revealed_lizard_count)
 	if easy_reveal:
-		var method: String = Utils.rand_value(["compare_by_row", "compare_by_column", "compare_by_region"])
+		var method: String = Utils.rand_value(["_compare_by_row", "_compare_by_column", "_compare_by_region"])
 		for card in level_cards.get_cards():
 			if _conflicting_lizard(card, method):
 				card.show_front()
@@ -182,7 +182,7 @@ func _conflicting_lizard(card: CardControl, method: String = "") -> CardControl:
 		methods = [method]
 	else:
 		# no comparator method was provided; find a lizard which conflicts in any of three ways
-		methods = ["compare_by_row", "compare_by_column", "compare_by_region"]
+		methods = ["_compare_by_row", "_compare_by_column", "_compare_by_region"]
 	
 	var pos := level_cards.get_cell_pos(card)
 	for lizard_card in _shown_lizard_cards():
@@ -256,7 +256,7 @@ func _on_level_cards_before_shark_found(shark_card: CardControl) -> void:
 	
 	# reveal all cards in the row/column/region
 	var shark_card_pos := level_cards.get_cell_pos(shark_card)
-	for compare_method in ["compare_by_row", "compare_by_column", "compare_by_region"]:
+	for compare_method in ["_compare_by_row", "_compare_by_column", "_compare_by_region"]:
 		if _conflicting_lizard(shark_card, compare_method):
 			for card in level_cards.get_cards():
 				var card_pos := level_cards.get_cell_pos(card)