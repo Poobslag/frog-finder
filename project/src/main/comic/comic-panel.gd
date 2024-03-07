0e88dcb52259defb43f4b305047a1e74b66dd239
Poobslag <aaron.pieper@gmail.com>
2024-03-07 14:52:43 -0500

    Extracted ComicPage.tscn, ComicPanel.tscn
    Rewrote comic page scenes to inherit ComicPage, ComicPanel. This reduces
    the amount of copy/paste code, and reduces the likelihood of bugs from
    things like layout differences between different manually created
    scenes.
    
    Rewrote ComicPanel to use Godot 4 'unique name' functionality.

--- project/src/main/comic/comic-panel.gd
+++ project/src/main/comic/comic-panel.gd
@@ -2,12 +2,6 @@
 extends Control
 ## Shows a panel within a comic page.
 
-## The node which renders the border of the comic panel.
-@onready var _frame := $Frame
-
-## The node which renders the inner contents of the comic panel.
-@onready var _contents := $Contents
-
 ## The modulate property to apply to both the border and inner contents of the comic panel.
 @export var panel_modulate: Color = Color.WHITE:
 	set(new_panel_modulate):
@@ -18,16 +12,10 @@ func _ready() -> void:
 	_refresh_panel_modulate()
 
 
-## Preemptively initializes onready variables to avoid null references.
-func _enter_tree() -> void:
-	_frame = $Frame
-	_contents = $Contents
-
-
 ## Applies our 'panel_modulate' property to the border and inner contents of the comic panel.
 func _refresh_panel_modulate() -> void:
 	if not is_inside_tree():
 		return
 	
-	_frame.self_modulate = panel_modulate
-	_contents.self_modulate = panel_modulate
+	%Frame.self_modulate = panel_modulate
+	%Contents.self_modulate = panel_modulate
