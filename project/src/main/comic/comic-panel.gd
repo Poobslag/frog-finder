2464ba8b209d7d7bc8801a240c29a1122a0b6694
Poobslag <aaron.pieper@gmail.com>
2024-05-23 13:06:29 -0400

    Workaround for 'unique animation library' issue
    When creating a new ComicPanel, the animations couldn't be edited
    because they were reusing the parent AnimationPlayer's AnimationPlayer.
    I've added some logic to the ComicPanel script so that when a ComicPanel
    is opened in the editor, it will duplicate the parent animation library.
    This lets us reuse the parent animations while still allowing changes.

--- project/src/main/comic/comic-panel.gd
+++ project/src/main/comic/comic-panel.gd
@@ -2,6 +2,8 @@
 extends Control
 ## Shows a panel within a comic page.
 
+const DEFAULT_ANIMATION_LIBRARY_PATH := "res://src/main/comic/default-frame-player-animation-library.tres"
+
 ## The modulate property to apply to both the border and inner contents of the comic panel.
 @export var panel_modulate: Color = Color.WHITE:
 	set(new_panel_modulate):
@@ -9,7 +11,31 @@ extends Control
 		_refresh_panel_modulate()
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		_ensure_unique_animation_library()
 	_refresh_panel_modulate()
+
+
+## Creates an editable copy of the default parent animation library, if necessary.
+##
+## The parent animation library is read-only, but also includes some useful defaults. This method lets us reuse them
+## while still allowing changes.
+func _ensure_unique_animation_library() -> void:
+	var animation_library: AnimationLibrary = %FramePlayer.get_animation_library("")
+	if animation_library.resource_path != DEFAULT_ANIMATION_LIBRARY_PATH:
+		# animation library is already unique; do not make another copy
+		return
+	
+	if get_tree().edited_scene_root == self:
+		# don't modify the ComicPanel scene itself; only modify descendents
+		return
+	
+	%FramePlayer.remove_animation_library("")
+	%FramePlayer.add_animation_library("", animation_library.duplicate(true))
+	notify_property_list_changed()
+	
+	var relative_path := get_tree().edited_scene_root.get_parent().get_path_to(self)
+	print("Duplicated animation library for editing: %s." % [relative_path])
 
 
 ## Applies our 'panel_modulate' property to the border and inner contents of the comic panel.
